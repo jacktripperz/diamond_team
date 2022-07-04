05964515d187112a8a25dcf7d468a349666c7aa3
jacktripperz <101054720+jacktripperz@users.noreply.github.com>
2022-07-04 09:46:06 +0200

    Fixed typo

--- diamond_cycle.py
+++ diamond_cycle.py
@@ -183,6 +183,6 @@ def run():
             run()
         else:
             print("********* TERMINATING *****************")
-            print("Expection occurred 5 times. Terminating!")
+            print("Exception occurred 5 times. Terminating!")
 
 run()
